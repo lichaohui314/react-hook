60ecf05e6ed878d1fbdad5932509484050a6c27a
a316760006 <316760006@qq.com>
2020-02-21 15:19:50 +0800

    'class组件'

---
+++ src/class组件2.js
@@ -0,0 +1,40 @@
+import React, { Component, useEffect, useState } from 'react';
+import { render } from 'react-dom';
+// 虚拟 dom 好处,只会更新改变的地方
+export default class Coutner extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { timer: new Date().toLocaleTimeString() }
+  }
+  // component 组件  Did 已完成 Mount 挂架  组件挂载完成 
+  componentDidMount() { // 只会执行一次 
+    // ajax请求 异步处理 
+    this.time = setInterval(() => {
+      this.setState({ timer: new Date().toLocaleTimeString() })
+    }, 1000);
+  }
+  // wiil 将要 un+加在一个单词上面标示取反  卸载 
+  componentWillUnmount() {
+    clearInterval(this.time)
+  }
+  render() {
+    return (
+
+      <div>
+        {this.state.timer}
+      </div>
+    )
+  }
+}
+function Coutner2() {
+  let [time, setTime] = useState(() => new Date().toLocaleTimeString())
+  useEffect(() => {
+    setInterval(()=>{
+      setTime(new Date().toLocaleTimeString());
+    },1000)
+  },[])
+  return (<>
+    {time}
+  </>)
+}
+render(<><Coutner /><Coutner2 /></>, window.root)
--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react'
+//usereducer 的logger 中间件 
+import React, { useReducer, useEffect } from 'react'
 import { render } from 'react-dom'
-// 自定义hooks  规则 use开头 hooks 钩子 
-// 自定义hooks 共用的只是逻辑  数据会单独生成新的副本  
-function useNumber() {
-  let [number, setnumber] = useState(0)
-  return [number, setnumber]
+let initialState = {
+  number: 1
 }
-function Parent1() {
-  let [number, setnumber] = useNumber()
-  return <>
-    <div>组件1{number} </div>
-    <button onClick={() => setnumber(number + 100)}>加100</button>
-  </>
-
+function reducer(state = {}, action) {
+  switch (action.type) {
+    case 'ADD':
+      return { number: state.number + 1 }
+    default:
+      break
+  }
 }
-function Parent2() {
-  let [number, setnumber] = useNumber()
-  return <>
-    <div>组件1 {number}</div>
-    <button onClick={() => setnumber(number + 500)}>加500</button>
-  </>
-
+// 日志中间件 
+function useLogger(reduer, initialState) {
+  let [state, dispacth] = useReducer(reduer, initialState) //这个dispath 是我们真正要派发的dispach 
+  function logdispatch(action) {
+    console.log(state, '老状态')
+    dispacth(action)
+  }
+  useEffect(() => {
+    console.log(state, '新状态')
+  }, [state])
+  return [state, logdispatch]
 }
-
-render(<><Parent1></Parent1><Parent2></Parent2></>, window.root)
+function Reducecompont() {
+  const [state, logdispatch] = useLogger(reducer, initialState)
+  return (
+    <div>
+      <span >{state.number}</span>
+      <button onClick={() => logdispatch({ type: 'ADD' })}>点击加一</button>
+    </div>
+  )
+}
+render(<Reducecompont></Reducecompont>, window.root)
